36576c524b1e50e62fafa580058d2a5d4b107d0a
digital opium <test@ismail.com>
2023-06-07 21:42:03 +0100

    file structure

--- src/App.js
+++ src/App.js
@@ -1,38 +1,10 @@
-// import { Routes, Route, BrowserRouter } from "react-router-dom";
-// import Train from "./components/ex1/Train";
-// import Voiture from "./components/ex3/Voiture";
-// import Courses from "./components/ex4/Cources";
-// import Menu from "./components/ex4/menu";
-// import Participant from "./components/ex4/participant";
-// import { useState } from "react";
 
-import Boisson from "./components/ex5/Boisson";
 
 function App() {
 
-//   const [participant,setParticipant] = useState([
-//     {id:1, nom:'Med', idCourse:'MarcheH'},
-//     {id:2, nom:'Rajae', idCourse:'MarcheF'}
-//   ]);
-
-//   const cources = [
-//     {id:'MarcheF', nom:'marche Femmes', categorie:'F', distance:'1000'},
-//     {id:'MarcheH', nom:'marche Hommes', categorie:'H', distance:'1000'},
-// ];
-
   return (
     <>
-    {/* <BrowserRouter>
-    <Routes>
-      <Route path="/" element={ <Menu /> } >
-        <Route path="/" element={ <Courses cources={cources} participant={participant} /> } />
-        <Route path="/newparticipant" element={ <Participant cources={cources} participant={participant} setParticipant={setParticipant} /> } />
-      </Route>
-    </Routes>
-    </ BrowserRouter> */}
-
-      <Boisson />
-
+      
     </>
   );
 }
