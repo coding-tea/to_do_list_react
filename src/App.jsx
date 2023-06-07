690c308ab079fc5d35a0f0331dae1893276a9775
digital opium <test@ismail.com>
2023-06-07 22:52:58 +0100

    task component complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
+import { useState } from 'react';
 import './app.css';
+import Add from './components/Add';
+import Task from './components/Task';
 
 function App() {
 
-  const tasks = [
+  const [tasks, setTasks] = useState([
     {id:1, task:'to do 1', done:false },
     {id:2, task:'to do 2', done:false },
     {id:3, task:'to do 3', done:false },
     {id:4, task:'to do 4', done:false },
-  ];
+  ]);
 
   return (
     <div className="app">
-      
+      <Add tasks={tasks} setTasks={setTasks} />
+      <Task tasks={tasks} setTasks={setTasks} />
     </div>
   );
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import './task.css';
 
 const Task = (props) => {
+
+    const doneOrUndo = (state, id) => {
+        const task = props.tasks.find(item => item.id === id);
+        let tasks = props.tasks.filter(item => item.id !== id);
+        // console.log(task);
+        if(state == 'done'){
+            task.done = true;
+            tasks.push(task);
+            props.setTasks(tasks);
+        }
+        else{
+            task.done = false;
+            tasks = [task, ...tasks];
+            props.setTasks(tasks);
+        }
+    };
+
+    const destroy = (id) => {
+        props.setTasks(props.tasks.filter(item => item.id !== id));
+    }
+
     return (
-        <div className='task'>
+        <div>
             {
-                props.tasks.map(item => <div key={item.id}>
+                props.tasks.map(item => <div key={item.id} className='task'>
                     <p className="info"> { item.task } </p>
-                    <button className={ (item.done)?'done':'undo' }> { (item.done)?'done':'undo'}  </button>
+                    <div className="cta">
+                        <button className={ (item.done)?'undo':'done' } onClick={() => doneOrUndo((item.done)?'undo':'done', item.id)}> { (item.done)? <i class="bi bi-x-circle-fill"></i> : <i class="bi bi-check-circle-fill"></i>}  </button>
+                        <button className='trash' type='button' onClick={() => destroy(item.id)}> <i class="bi bi-trash3"></i> </button>
+                    </div>
                 </div>)
             }
         </div>
