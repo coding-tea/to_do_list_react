789d5c06fbf047f06d1c3bb6bbf18b76c0fa32fc
digital opium <test@ismail.com>
2023-06-07 22:01:37 +0100

    styling app class

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './app.css';
+
+function App() {
+
+  const tasks = [
+    {id:1, task:'to do 1', done:false },
+    {id:2, task:'to do 2', done:false },
+    {id:3, task:'to do 3', done:false },
+    {id:4, task:'to do 4', done:false },
+  ];
+
+  return (
+    <div className="app">
+      
+    </div>
+  );
+}
+
+export default App;
